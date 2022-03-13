1c5892c1901d1c84b752af65a7e6db9bb02b5051
vanshika-sood <vanshika29sood@gmail.com>
2022-03-14 01:27:52 +0530

    modify payment success page

--- pages/paymentSuccess.js
+++ pages/paymentSuccess.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import  Link  from 'next/link'
+import bodyParser from "body-parser";
+import { promisify } from "util";
 import styles from '../styles/razorCallback.module.css'
 import { faCheckDouble } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function PaymentSuccess() {
+
+const getBody = promisify(bodyParser.urlencoded());
+
+export async function getServerSideProps({ req, res }) {
+  if (req.method === "POST") {
+    await getBody(req, res);
+  }
+
+  return {
+    props: {
+      orderId: req.body.razorpay_order_id
+    }
+  };
+}
+
+function PaymentSuccess(props) {
   return (
     <div>
       <div className={styles.container}>
@@ -12,9 +29,10 @@ function PaymentSuccess() {
                   <FontAwesomeIcon icon={faCheckDouble} className={styles.logoz}/>{' '}
         </div>
         <div className={styles.success}>Payment Successful</div>
+        <div className={styles.success}>Order ID : {props.orderId}</div>
         <div className={styles.back}>
           Click here to go back to
-          <Link href="/pitchersPilot" className={styles.link}>Pitcher&apos;s Pilot</Link>
+          <Link href="/id/portal" className={styles.link}>E-Cell BPHC Internship Drive</Link>
         </div>
       </div>
     </div>
@@ -22,3 +40,5 @@ function PaymentSuccess() {
 }
 
 export default PaymentSuccess;
+
+

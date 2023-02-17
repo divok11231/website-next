5bd60672a63780e0d15ac4ef5e93babe473a89d8
Manan <manangupta03@gmail.com>
2023-02-17 22:40:18 +0530

    new

--- components/gr/QA.js
+++ components/gr/QA.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import styles from '../../styles/GR/QA.module.css'
 import Image from 'next/image'
-import Qm from '../../assets/images/Qm.png'
+import info from '../../assets/images/info.png'
 export default function QA({ question, answer, classy, questionid }) {
   return (
     <>
       <div className={styles.qa}>
         <div className={styles.accordian}>
-          <div classname={styles.img}>
-            {/* <Image className={styles.icon} src={Qm} width={30} height={25} /> */}
+          <div>
+            <Image className={styles.icon} src={info} width={30} height={25} />
           </div>
           <div className={styles.h}> {question} </div>
         </div>

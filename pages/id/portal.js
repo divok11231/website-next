c8f3a9afb422ed2a48e3920caa715903e63c6084
Neon_99 <f20210896@hyderabad.bits-pilani.ac.in>
2023-03-22 18:03:21 +0530

    Internship drive video update

--- pages/id/portal.js
+++ pages/id/portal.js
@@ -82,7 +82,7 @@ export default function Home() {
           <div className={styles.video}>
             <ReactPlayer
               // playing={playing}
-              url="https://youtu.be/d7V9pdM7e58"
+              url="https://youtu.be/hood68TAnv0"
               width="100%"
               height="100%"
               controls={false}
